f13da93ef615a571bbce0154310560bf4ce963e7
sami638341 <urrehmansami596@gmail.com>
2026-07-16 14:00:53 +0500

    Update details.js

--- details.js
+++ details.js
@@ -1,9 +1,12 @@
-const data = JSON.parse(localStorage.getItem("dayData"));
+let data = JSON.parse(localStorage.getItem("dayData"));
 
 document.getElementById("details").innerHTML = `
   <h2>${data.date}</h2>
-  <p>Max Temp: ${data.day.maxtemp_c}°C</p>
-  <p>Min Temp: ${data.day.mintemp_c}°C</p>
-  <p>Humidity: ${data.day.avghumidity}%</p>
-  <p>Condition: ${data.day.condition.text}</p>
+  <h1>${data.day.avgtemp_c}°C</h1>
+  <img src="https:${data.day.condition.icon}">
+  <p>${data.day.condition.text}</p>
+
+  <p>🌡 Max: ${data.day.maxtemp_c}°C</p>
+  <p>❄ Min: ${data.day.mintemp_c}°C</p>
+  <p>💧 Humidity: ${data.day.avghumidity}%</p>
 `;
